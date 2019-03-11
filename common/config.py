2c12616d6b505e75b9ebfc7a59e2c34d0b20fa3d
Ehsan <30546099+edoost@users.noreply.github.com>
2019-03-11 13:20:43 +0330

    Create config.py

---
+++ common/config.py
@@ -0,0 +1,14 @@
+input_max_length = 500
+output_max_length = 10
+
+num_words = 100000
+
+source_data_train = './data/source_data.train'
+target_data_train = './data/target_data.train'
+source_data_dev = './data/source_data.dev'
+target_data_dev = './data/target_data.dev'
+source_data_test = './data/source_data.test'
+target_data_test = './data/target_data.test'
+
+learning_rate = 0.001
+clip_norm = 5.0
