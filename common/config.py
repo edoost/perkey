a361cc88dc481e2c57fa6d30e763ac20773dfbe7
Ehsan <30546099+edoost@users.noreply.github.com>
2019-03-11 13:23:20 +0330

    Update config.py

--- common/config.py
+++ common/config.py
@@ -3,6 +3,8 @@ output_max_length = 10
 
 num_words = 100000
 
+
+# directories
 source_data_train = './data/source_data.train'
 target_data_train = './data/target_data.train'
 source_data_dev = './data/source_data.dev'
@@ -10,5 +12,8 @@ target_data_dev = './data/target_data.dev'
 source_data_test = './data/source_data.test'
 target_data_test = './data/target_data.test'
 
+# learning hyperparameters
+num_rounds = 100
+num_steps = 1000
 learning_rate = 0.001
 clip_norm = 5.0
